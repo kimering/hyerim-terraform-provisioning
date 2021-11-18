94758f5e4c46425088a1faf271cc9e06a55bcb7f
kimering <k.hyerim96@gmail.com>
2021-11-18 14:04:46 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0aab712d6363da7f9" # region sydney
   instance_type = var.test
-  key_name = 
+  key_name = var.key
   associate_public_ip_address = false
   subnet_id = var.subnet_id
   vpc_security_group_ids = [
